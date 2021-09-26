199046e9a882601bccf2a94ea8ad66ad6227a2ec
agemperador <emperadoragustin@gmail.com>
2021-09-26 14:13:09 -0300

    Componente de info y links del footer

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,18 +1,27 @@
 import React from 'react'
 import ContactForm from '../contact-form/contact-form'
 import styles from './footer.module.scss'
+import InfoFooter from './info-footer/info-footer'
 
 const Footer = () => {
     return (
         <div className= {styles.container}>
-            Footer
+            
             <div className={styles.grid}>
                 
+                
+                <div className={styles.info}>
+                    <InfoFooter/>
+                </div>
+                <div className={styles.info}>
+                    <InfoFooter/>
+                </div>
+
                 <div className={styles.form}>
-                    Formulario
+                    
                     <ContactForm/>
                 </div>
-              
+            
             </div>
             <div className= {styles.footer}>
                 Copyright
