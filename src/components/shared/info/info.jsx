3269fe4b706270424df2cf96050643602111091a
lukitinchox@gmail.com <22123060+LucasTurco92@users.noreply.github.com>
2021-10-28 21:47:16 -0300

    magic component and interceptor hook added

--- src/components/shared/info/info.jsx
+++ src/components/shared/info/info.jsx
@@ -1,11 +1,14 @@
 import styles from './info.module.scss';
+import MagicComponent from '../magic-component/magic-component';
 const Info = ({title,subtitle,paragraph}) =>{
 
     return(
         <div className={styles.container} >
             <h2 className={styles.title}>{title}</h2>
-            <h3>{subtitle}</h3>
-            <p>{paragraph}</p>
+            <MagicComponent>
+                <h3>{subtitle}</h3>
+                <p>{paragraph}</p>
+            </MagicComponent>
         </div>
     )
 }
---
+++ src/components/shared/magic-component/magic-component.jsx
@@ -0,0 +1,15 @@
+import styles from "./magic-component.module.scss";
+import { useRef } from 'react';
+import useOnScreen from '../../../hooks/intersectionObserver';
+
+const MagicComponent = ({children}) => {
+  const ref = useRef();
+  const onScreen = useOnScreen(ref, "");
+  return (
+    <div  className={onScreen ? styles.show : ''}  ref={ref}> 
+        {children}
+    </div>
+  )
+}
+
+export default MagicComponent;
