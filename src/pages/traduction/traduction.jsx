29ddac1b0bd4c1c4d4973472d9487ef981705aa7
lukitinchox@gmail.com <22123060+LucasTurco92@users.noreply.github.com>
2021-12-29 02:02:52 -0300

    traduction page finished

--- src/pages/traduction/traduction.jsx
+++ src/pages/traduction/traduction.jsx
@@ -3,20 +3,50 @@ import styles from './traduction.module.scss';
 import Info from '../../components/shared/info/info';
 import { useTranslation } from "react-i18next";
 import Slideshow from './../../components/shared/slide-show/slide-show.jsx';
+import TraductionSlider from '../../components/traduction-slider/traduction-sliders.jsx';
 
 const Traduction = () => {
     const { t } = useTranslation();
-    const listTraductions = [{key:1,name:'1',url:'traductions/dias-de-invierno.jpg'},
-    {key:2,name:'2',url:'traductions/corta.jpg'},
-    {key:3,name:'dsafgdgfsgdfsdgfdfgsgdfsgdsfgdfs',url:'traductions/cuando-la-comida-es-tu-enemiga.jpg'},
-    {key:4,name:'dsafgdgfsgdfsdgfdfgsgdfsgdsfgdfs',url:'traductions/cueva-de-bandidos.jpg'},
-    {key:5,name:'5',url:'traductions/la-conexion-bellarosa.jpg'},
-    {key:6,name:'dsafgdgfsgdfsdgfdfgsgdfsgdsfgdfs',url:'traductions/mision-en-venecia.jpg'},
-    {key:7,name:'7',url:'traductions/obras-espirituales-de-miser.jpg'},
-    {key:8,name:'dsafgdgfsgdfsdgfdfgsgdfsgdsfgdfs',url:'traductions/tiempo-de-morir.jpg'},
-    {key:9,name:'9',url:'traductions/una-plaza-en-moscu.jpg'},
-    {key:10,name:'dsafgdgfsgdfsdgfdfgsgdfsgdsfgdfs',url:'traductions/viaje-al-infierno.jpg'}];
+    const listTraductions = {
+        color:'violet',
+        title:'Libros traducidos',
+        list:[{key:1,name:'Días de invierno',url:'traductions/dias-de-invierno.jpg'},
+            {key:2,name:'Corta madera y ve a buscar agua',url:'traductions/corta.jpg'},
+            {key:3,name:'Cuando la comida es tu enemiga',url:'traductions/cuando-la-comida-es-tu-enemiga.jpg'},
+            {key:4,name:'Cueva de bandidos',url:'traductions/cueva-de-bandidos.jpg'},
+            {key:5,name:'La conexión Bellarosa',url:'traductions/la-conexion-bellarosa.jpg'},
+            {key:6,name:'Misión en Venecia',url:'traductions/mision-en-venecia.jpg'},
+            {key:7,name:'Las obras espirituales de misericordiam, Ediciones PAULINAS',url:'traductions/obras-espirituales-de-miser.jpg'},
+            {key:8,name:'Tiempo de morir',url:'traductions/tiempo-de-morir.jpg'},
+            {key:9,name:'Una Plaza en Moscú',url:'traductions/una-plaza-en-moscu.jpg'},
+            {key:10,name:'Viaje al infierno, Editorial PLANETA, 1992',url:'traductions/viaje-al-infierno.jpg'}
+        ]};
     
+    const othersTraductions = {
+        color:'ocean',
+        title:'Otras traducciones publicadas',
+        list:[{key:1,name:'Mealtime Memo Series',url:'traductions/mealtime-memo.jpg'},
+            {key:2,name:'Justice of Justices',url:'traductions/justice-of-justices.jpg'},
+            {key:3,name:'Líneas directrices de la OCDE para empresas multinacionales',url:'traductions/lineas-directrices.jpg'}
+        ]};
+
+    const projectTraductions = {
+        color:'sunset',
+        title:'Gestión de proyectos de traducción en equipo',
+        list:[{key:1,name:'Diana, su verdadera historia',url:'traductions/diana-verdadera-historia.jpg'},
+    {key:2,name:'Parques Nacionales de Sudamérica',url:'traductions/south-america.jpg'},
+    {key:3,name:'50 cosas que Ud. puede hacer para salvar la Tierra',url:'traductions/50-cosas-para-salvar-la-tie.jpg'},
+    {key:4,name:'Curación fatal',url:'traductions/curacion-fatal.jpg'},
+    {key:5,name:'El mundo perdido de Jurassic Park',url:'traductions/mundo-perdido.jpg'},
+    {key:6,name:'Cómo dejar de fumar',url:'traductions/dejar-de-fumar.jpg'},
+    {key:7,name:'Ah...Todo lo que no se puede decir con palabrash',url:'traductions/ah.jpg'},
+    {key:8,name:'El libro de los juguetes',url:'traductions/toy.jpg'},
+    {key:9,name:'Los signos del zodíaco',url:'traductions/zoodiaco.jpg'},
+    {key:10,name:'El libro de los jugos',url:'traductions/jugos.jpg'},
+    {key:11,name:'Informe del INTAL',url:'traductions/intal.jpg'}]};
+
+    const list = [ listTraductions,othersTraductions,projectTraductions ];
+
     const paragraph =()=>{
         return(
             <div className={styles.paragraphContainer}>
@@ -45,7 +75,16 @@ const Traduction = () => {
             title={t("traduction").toUpperCase()} 
             paragraph={paragraph()}
             />
-            <Slideshow time={1} items={listTraductions} wide={160}/>
+            <div className={styles.traductions}>
+                {
+                    list.map(x=>(
+                        <TraductionSlider time={2.5} key={x.title} title={x.title} color={x.color} wide={160}>
+                            <Slideshow time={2.5} key={x.title} items={x.list} wide={160}/>
+                        </TraductionSlider>
+                        )
+                    )
+                }
+            </div>
         </div>
     )
 }
