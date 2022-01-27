b2c6b727703530e81a6a27c7a4aa8a3195750c4e
agemperador <emperadoragustin@gmail.com>
2022-01-27 17:09:03 -0300

    redirect added to netlify

--- src/components/header/flags/flags.jsx
+++ src/components/header/flags/flags.jsx
@@ -1,4 +1,4 @@
-import React,{ useContext,useState } from "react";
+import React from "react";
 import styles from "./flags.module.scss";
 //import { changeLang }  from "../../../translations/i18n";
 import { useTranslation } from 'react-i18next';
