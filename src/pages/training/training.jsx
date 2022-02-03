ad4d06064c40de5b631bdc88a3f28177e7cb3b87
agemperador <emperadoragustin@gmail.com>
2022-02-03 17:56:40 -0300

    training page

--- src/pages/training/training.jsx
+++ src/pages/training/training.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React,{useState} from 'react'
 import Banner from '../../components/header/banner/banner';
 import { useTranslation } from "react-i18next";
 import Info from '../../components/shared/info/info';
 import {TrainingContainer} from './training.styles'
+import TrainingList from './training-list/training-list';
+import TeamList from '../../components/shared/team-list/team-list';
 
 const Training = () => {
     const { t } = useTranslation();
+
+    const [trainingOption, setTrainingOption] = useState('curso');
 
     const paragraph = () => {
         return(<div>
@@ -13,11 +17,23 @@ const Training = () => {
         </div>)
     };
 
+    const teachers = [
+        'Olga Álvarez – Coordinadora de EXCELTI',
+        'Elisa Bianchi',
+        'Violeta Harfuch',
+        'Gabriela Yáñez'
+    ]
+
     return (
         <TrainingContainer>
             {/* <Banner src={'header/training-page.jpg'} title={t("training").toUpperCase()} /> */}
-            <Info paragraph={paragraph()}/>
-
+            <Info className='info' paragraph={paragraph()}/>
+            <TrainingList>
+            </TrainingList>
+            <div className='content'>
+                <h2>Contenido de la capacitación</h2>
+            </div>
+            <TeamList teamTitle={'docentes'} teamList={teachers}/>
         </TrainingContainer>
     )
 }
