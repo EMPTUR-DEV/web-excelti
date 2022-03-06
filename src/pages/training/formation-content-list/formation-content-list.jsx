4b73aa18a2ec827f5e5181aa10ac8c155d77b73d
agemperador <emperadoragustin@gmail.com>
2022-03-06 15:32:04 -0300

    scroll details training page

--- src/pages/training/formation-content-list/formation-content-list.jsx
+++ src/pages/training/formation-content-list/formation-content-list.jsx
@@ -36,17 +36,18 @@ const FormationContentList = ({ formationItems}) => {
             scrollToRef()
         }
     }
-    console.log(formationItems.key, itemActive)
+    
     return (
         <FormationContentListContainer itemActive={itemActive}>
             <FormationList ref={refScroll} arrowActiveStart = {!onScreenStart} arrowActiveEnd = {!onScreenEnd}>
             {formationItems.key ==='curso' ?formationItems.content.map((formationItem)=> 
-                
-                    <ItemContent ref={formationItem.key ===6 ? refEnd : formationItem.key===1 ? refStart :null }  
+                {
+                    
+                 return(   <ItemContent ref={formationItem.key ===6 ? refEnd : formationItem.key===1 ? refStart :null }  
                                 itemActive={itemActive===formationItem.key} 
                                 onClick={()=>handleItemClick(formationItem.key)} key={formationItem.key}>
                     <FormationButton key={formationItem.key} ><h4>{formationItem.title}</h4>  </FormationButton>
-                </ItemContent>):''}
+                </ItemContent>)}):''}
             </FormationList>
                 {
                 formationItems.key ==='curso' ?
